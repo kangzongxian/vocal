abad91809ed57243623bd86e227dd3ae058a8d87
Zong Xian <zongxian.kang@kabam.ai>
2023-05-10 21:09:23 +0800

    Add functionality to get user data

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,2 +1,41 @@
 // [username] means /anything will route to that
 // However, static routes have priority
+
+import { getUserWithUsername, postToJSON } from '../../lib/firebase';
+import UserProfile from '../../components/UserProfile';
+import PostFeed from '../../components/PostFeed';
+
+// SSR
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  // Fetch user documents
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  );
+}
