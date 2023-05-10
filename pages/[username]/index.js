259d15d2d67cadb968ecf08a7d8c4adb0852f58f
Zong Xian <zongxian.kang@kabam.ai>
2023-05-10 23:22:36 +0800

    Add redirection to 404 page if no data

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -12,6 +12,13 @@ export async function getServerSideProps({ query }) {
   // Fetch user documents
   const userDoc = await getUserWithUsername(username);
 
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
+
   // JSON serializable data
   let user = null;
   let posts = null;
