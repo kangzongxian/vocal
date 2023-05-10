416e38c60b1e7560f04e730a828752cdf5b2bf39
Zong Xian <zongxian.kang@kabam.ai>
2023-05-10 23:29:56 +0800

    Add metatags

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,8 +1,12 @@
 // Using [slug] means it is dynamic
+
+import Metatags from "@/components/Metatags";
+
 // E.g Can render /admin/hello or /admin/tesla
 export default function AdminPostEdit({ }) {
   return (
     <main>
+      <Metatags title="admin page" />
       <h1>Edit Post</h1>
     </main>
   )
