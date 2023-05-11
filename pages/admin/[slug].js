5aa2b3a28f37a3f1c82035158238b98888dafc9d
Zong Xian <zongxian.kang@kabam.ai>
2023-05-11 11:58:10 +0800

    Add edit form functionality

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,13 +1,98 @@
-// Using [slug] means it is dynamic
+import styles from '../../styles/Admin.module.css';
+import AuthCheck from '../../components/AuthCheck';
+import { firestore, auth, serverTimestamp } from '../../lib/firebase';
 
-import Metatags from "@/components/Metatags";
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
-// E.g Can render /admin/hello or /admin/tesla
-export default function AdminPostEdit({ }) {
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useForm } from 'react-hook-form';
+import ReactMarkdown from 'react-markdown';
+import Link from 'next/link';
+import toast from 'react-hot-toast';
+
+export default function AdminPostEdit(props) {
   return (
-    <main>
-      <Metatags title="admin page" />
-      <h1>Edit Post</h1>
+    <AuthCheck>
+        <PostManager />
+    </AuthCheck>
+  );
+}
+
+function PostManager() {
+  // See if is preview or not
+  const [preview, setPreview] = useState(false);
+
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const postRef = firestore.collection('users').doc(auth.currentUser.uid).collection('posts').doc(slug);
+  const [post] = useDocumentData(postRef);
+
+  return (
+    <main className={styles.container}>
+      {/* See if post is ready */}
+      {post && (
+        <>
+          <section>
+            <h1>{post.title}</h1>
+            <p>ID: {post.slug}</p>
+
+            <PostForm postRef={postRef} defaultValues={post} preview={preview} />
+          </section>
+
+          <aside>
+          <h3>Tools</h3>
+            <button onClick={() => setPreview(!preview)}>{preview ? 'Edit' : 'Preview'}</button>
+            <Link href={`/${post.username}/${post.slug}`}>
+              <button className="btn-blue">Live view</button>
+            </Link>
+          </aside>
+        </>
+      )}
     </main>
-  )
+  );
+}
+
+function PostForm({ defaultValues, postRef, preview }) {
+  // When on Change, will re-render
+  const { register, handleSubmit, reset, watch } = useForm({ defaultValues, mode: 'onChange' });
+
+  // Update the form in Firestore too
+  const updatePost = async ({ content, published }) => {
+    await postRef.update({
+      content,
+      published,
+      updatedAt: serverTimestamp(),
+    });
+
+    reset({ content, published });
+
+    toast.success('Post updated successfully!')
+  };
+
+  return (
+    <form onSubmit={handleSubmit(updatePost)}>
+      {preview && (
+        <div className="card">
+          <ReactMarkdown>{watch('content')}</ReactMarkdown>
+        </div>
+      )}
+
+      {/* See if we are in Preview Mode or not */}
+      <div className={preview ? styles.hidden : styles.controls}>
+  
+        <textarea name="content" ref={register}></textarea>
+
+        <fieldset>
+          <input className={styles.checkbox} name="published" type="checkbox" ref={register} />
+          <label>Published</label>
+        </fieldset>
+
+        <button type="submit" className="btn-green">
+          Save Changes
+        </button>
+      </div>
+    </form>
+  );
 }
